0b470a5322d4fd6053f989800e620518342aa822
Phillip Kravtsov <phillip@kravtsov.net>
2023-12-04 15:15:40 -0800

    minor updates

--- token-transfer.py
+++ token-transfer.py
@@ -17,13 +17,16 @@ class TrieNode:
         char: Optional[str] = None,
         is_word: bool = False,
         prob: Optional[float] = None,
-        log_prob: Optional[float] = None,
     ):
         self.char = char
         self.is_word = is_word
         self.prob = prob
-        self.log_prob = log_prob
         self.children: Dict[str, TrieNode] = {}
+
+    @property
+    def log_prob(self) -> float:
+        assert self.prob is not None
+        return np.log(self.prob)
 
     def add_child(self, child: "TrieNode"):
         assert child.char not in self.children and child.char is not None
@@ -34,10 +37,10 @@ class TrieNode:
             indents = [0]
 
         indent_string = "|".join([" " * indent for indent in indents])
-        info_string = str(round(self.prob, 4)) if self.prob is not None else ""
+        info_string = f"{self.prob:.4f}" if self.prob is not None else ""
         char_string = self.char
-        if self.char == '\n':
-            char_string = '\\n'
+        if self.char == "\n":
+            char_string = "\\n"
         print(indent_string + f"{char_string} {info_string}")
 
         indents = indents[:]
@@ -80,12 +83,6 @@ class TrieNode:
             p = p.children[char]
         assert p.is_word
         return p
-
-    def logify(self):
-        if self.prob:
-            self.log_prob = np.log(self.prob)
-        for child in self.children.values():
-            child.logify()
 
 
 def _add_probs(root: TrieNode, probs: List[Tuple[str, float]]):
@@ -178,7 +175,6 @@ def token_transfer(
     _fold_empty_nodes(root)
 
     root.pprint_node()
-    root.logify()
 
     cur = root
     target_token_log_probs = []
@@ -215,92 +211,81 @@ def test():
         source_log_probs.append(np.log(stp[0]))
     target_token_log_probs = token_transfer(source_tokens, target_tokens, probs)
     assert np.allclose(np.sum(target_token_log_probs), np.sum(source_log_probs))
-    
+
+
 def from_openai(response_logprobs, target_tokens):
     # add implicit bos token
-    source_tokens: List[str] = ['bos'] + response_logprobs['tokens']
-    target_tokens = ['bos'] + target_tokens[:]
+    source_tokens: List[str] = ["bos"] + response_logprobs["tokens"]
+    target_tokens = ["bos"] + target_tokens[:]
     probs_struct: List[List[Tuple[str, float]]] = []
-    for response_token_logprobs in response_logprobs['top_logprobs']:
-        probs_struct.append([(tok, np.exp(lp)) for tok, lp in response_token_logprobs.items()])
+    for response_token_logprobs in response_logprobs["top_logprobs"]:
+        probs_struct.append(
+            [(tok, np.exp(lp)) for tok, lp in response_token_logprobs.items()]
+        )
 
     target_logp = token_transfer(source_tokens, target_tokens, probs_struct)
     print(np.sum(target_logp))
-    print(np.sum(response_logprobs['token_logprobs']))
+    print(np.sum(response_logprobs["token_logprobs"]))
     return target_logp
+
 
 if __name__ == "__main__":
     data = {
-        "tokens": [
-          "\n\n",
-          "This",
-          " is",
-          " a",
-          " test",
-          "."
-        ],
+        "tokens": ["\n\n", "This", " is", " a", " test", "."],
         "token_logprobs": [
-          -0.86915743,
-          -0.35828337,
-          -0.008103862,
-          -0.00550173,
-          -0.0014032064,
-          -0.06300423
+            -0.86915743,
+            -0.35828337,
+            -0.008103862,
+            -0.00550173,
+            -0.0014032064,
+            -0.06300423,
         ],
         "top_logprobs": [
-          {
-            "\n\n": -0.86915743,
-            "\n": -1.5560223,
-            " string": -3.156324,
-            " sentence": -3.2896361,
-            " of": -3.7075934
-          },
-          {
-            "This": -0.35828337,
-            "\n": -1.847051,
-            "\n\n": -3.0329373,
-            " This": -4.944918,
-            "I": -4.9773717
-          },
-          {
-            " is": -0.008103862,
-            " sentence": -5.2877955,
-            "\n": -7.06451,
-            " statement": -7.886844,
-            "\n\n": -8.535557
-          },
-          {
-            " a": -0.00550173,
-            " just": -6.2743807,
-            " an": -6.3107214,
-            " only": -7.745223,
-            " not": -8.000508
-          },
-          {
-            " test": -0.0014032064,
-            " sentence": -7.599525,
-            " ": -8.814065,
-            " te": -8.949878,
-            " sample": -9.242383
-          },
-          {
-            ".": -0.06300423,
-            ".\n": -3.285281,
-            "<|endoftext|>": -4.3656716,
-            " ": -5.737307,
-            ",": -5.822476
-          }
+            {
+                "\n\n": -0.86915743,
+                "\n": -1.5560223,
+                " string": -3.156324,
+                " sentence": -3.2896361,
+                " of": -3.7075934,
+            },
+            {
+                "This": -0.35828337,
+                "\n": -1.847051,
+                "\n\n": -3.0329373,
+                " This": -4.944918,
+                "I": -4.9773717,
+            },
+            {
+                " is": -0.008103862,
+                " sentence": -5.2877955,
+                "\n": -7.06451,
+                " statement": -7.886844,
+                "\n\n": -8.535557,
+            },
+            {
+                " a": -0.00550173,
+                " just": -6.2743807,
+                " an": -6.3107214,
+                " only": -7.745223,
+                " not": -8.000508,
+            },
+            {
+                " test": -0.0014032064,
+                " sentence": -7.599525,
+                " ": -8.814065,
+                " te": -8.949878,
+                " sample": -9.242383,
+            },
+            {
+                ".": -0.06300423,
+                ".\n": -3.285281,
+                "<|endoftext|>": -4.3656716,
+                " ": -5.737307,
+                ",": -5.822476,
+            },
         ],
-        "text_offset": [
-          18,
-          20,
-          24,
-          27,
-          29,
-          34
-        ]
+        "text_offset": [18, 20, 24, 27, 29, 34],
     }
-    
-    target_tokens = ['\n\nThis', ' is a', ' test.']
-    from_openai(data, target_tokens)
 
+    target_tokens = ["\n\nThis", " is a", " test."]
+    from_openai(data, target_tokens)
